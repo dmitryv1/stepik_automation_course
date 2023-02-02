c18cf8c9c2ea87d70c9705c0ec2298568ea0dad3
dmitry.v2 <dmitry.v@ramotion.agency>
2023-02-02 18:16:24 +0300

    Work with marked test. Add some new files

--- test.py
+++ test.py
@@ -1,16 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import pytest
 
-browser = webdriver.Chrome()
-link = "https://stepik.org/lesson/184253/step/2?unit=158843"
-browser.get(link)
-browser.execute_script("alert('Robots at work');")
-alert = browser.switch_to.alert
-time.sleep(3)
-alert_text = alert.text
-print(alert_text)
-alert.accept()
-alert1 = browser.execute_script("alert('Woohoo');")
-browser.switch_to.ale
-time.sleep(3)
+@pytest.mark.xfail(strict=True)
+def test_succeed():
+    assert True
+
+
+@pytest.mark.xfail
+def test_not_succeed():
+    assert False
+
+
+@pytest.mark.skip
+def test_skipped():
+    assert False
